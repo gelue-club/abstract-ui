58cd188aa6a3709bb27eaaaeb128cddc2ee851f7
Tony Stark <swzyocowboy@icloud.com>
2019-11-20 20:45:32 +0800

    功能: :gear: 添加 `<BitmapIcon />` 位图图标组件

---
+++ src/components/BitmapIcon/BitmapIcon.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import cn from 'classnames';
+import { useMediaQuery } from 'react-responsive';
+import merge from 'deepmerge';
+
+function BitmapIcon({ size, src, xsrc, className, style = {} }) {
+  const _size = size.trim().split(' ');
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
+
+  return (
+    <i
+      className={cn('bitmap-icon', className)}
+      style={merge.all([
+        {
+          display: 'block',
+          backgroundImage: `url("${isRetina ? xsrc : src}")`,
+          backgroundPosition: 'center center',
+          backgroundRepeat: 'no-repeat',
+        },
+        _size.length === 1
+          ? {
+              width: _size,
+              height: _size,
+              backgroundSize: _size,
+            }
+          : {
+              width: _size[0],
+              height: _size[1],
+              backgroundSize: `${_size[0]} ${_size[1]}`,
+            },
+        style,
+      ])}
+    />
+  );
+}
+
+export default BitmapIcon;
---
+++ src/components/BitmapIcon/BitmapIcon.stories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Circle from '../Circle/Circle';
+import XyCenter from '../XyCenter/XyCenter';
+
+import BitmapIcon from './BitmapIcon';
+
+import icon from './icon.png';
+import icon2x from './icon@2x.png';
+
+storiesOf('<BitmapIcon />', module)
+  .add('位图图标', () => (
+    <Box>
+
+      <BitmapIcon
+        size="28px 22px"
+        src={icon}
+        xsrc={icon2x}
+      />
+
+    </Box>
+  ));
+
+function Box({ children }) {
+  return (
+    <Circle size="50px" style={{ position: 'relative', background: '#363636' }}>
+      <XyCenter>
+
+        { children }
+
+      </XyCenter>
+    </Circle>
+  );
+}
