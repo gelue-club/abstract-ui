dc8562e238b3b9131787c0faf8013f6192216fdf
Tony Stark <swzyocowboy@icloud.com>
2019-11-21 19:12:09 +0800

    功能: :gear: 更新 `<CtxMenu />` 右键面板菜单组件演示

--- src/components/CtxMenu/CtxMenu.stories.js
+++ src/components/CtxMenu/CtxMenu.stories.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import ShrinkPanel from '../ShrinkPanel/ShrinkPanel';
+import CongruentGrid from '../CongruentGrid/CongruentGrid';
 import CtxMenu from './CtxMenu';
 
-// 链接盒子
-storiesOf('<CtxMenu />', module).add('右键按钮', () => (
-  <CtxMenu to="https://qipao.app" />
-));
+storiesOf('<CtxMenu />', module)
+  .add('右键按钮', () => <CtxMenu />)
+  .add('使用网格组织的右键按钮', () => (
+    <ShrinkPanel radius="6px" padding="15px" bgColor="#2D2F2F">
+      <CongruentGrid
+        width="210px"
+        rowHeight="112px"
+        columnWidth="70px"
+        style={{ backgroundColor: '#2D2F2F' }}
+      >
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+        <CtxMenu />
+      </CongruentGrid>
+    </ShrinkPanel>
+  ));
