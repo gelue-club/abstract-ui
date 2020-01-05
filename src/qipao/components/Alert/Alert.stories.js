5ccdf3362eab0917be8e45fe5997a0374b5604b2
Tony Stark <swzyocowboy@icloud.com>
2020-01-05 09:24:04 +0800

    体验: :children_crossing: 支持在 Storybook 中变换组件属性，使用 `@storybook/addon-knobs` 实现

---
+++ src/qipao/components/Alert/Alert.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+import Alert from './Alert';
+
+storiesOf('组件.<Alert />', module)
+  .addDecorator(withKnobs)
+  .add('提示信息', () => (
+    <Alert padding={text('内边距', '16px 16px')}>
+      {text('消息', '验证码不存在')}
+    </Alert>
+  ));
