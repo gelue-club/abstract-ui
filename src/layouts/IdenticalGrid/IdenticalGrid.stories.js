1c64d346a20d1395d1c49fe9931ec898026d9421
iYoWei <swzyocowboy@icloud.com>
2020-06-06 01:13:20 +0800

    体验: :children_crossing: 为 `<IdenticalGrid />` 添加示例

--- src/layouts/IdenticalGrid/IdenticalGrid.stories.js
+++ src/layouts/IdenticalGrid/IdenticalGrid.stories.js
@@ -6,14 +6,31 @@ import IGItem from 'layouts/IGItem';
 
 import IdenticalGrid from './IdenticalGrid';
 
-storiesOf('布局/等距、等比例缩放', module).add('默认', () => (
-  <IdenticalGrid gap={16} columnCount={6} width={380}>
-    {times(16, index => (
-      <IGItem key={`${index}-`} className="inputting">
-        <div
-          style={{ width: '100%', height: '100%', backgroundColor: 'orange' }}
-        />
-      </IGItem>
-    ))}
-  </IdenticalGrid>
-));
+import './demo.css';
+
+storiesOf('布局/等距、等比例缩放', module)
+  .add('默认', () => (
+    <IdenticalGrid gap={16} columnCount={6} width={380}>
+      {times(16, index => (
+        <IGItem key={`${index}-`} className="inputting">
+          <div
+            style={{ width: '100%', height: '100%', backgroundColor: 'orange' }}
+          />
+        </IGItem>
+      ))}
+    </IdenticalGrid>
+  ))
+  .add('示例：一组数字', () => (
+    <IdenticalGrid
+      gap={7}
+      columnCount={10}
+      width={300}
+      className="demo-numbers-group"
+    >
+      {times(10, index => (
+        <IGItem key={`${index}-`}>
+          { index }
+        </IGItem>
+      ))}
+    </IdenticalGrid>
+  ));
